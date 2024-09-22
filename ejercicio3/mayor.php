b77a484eceae9747b221c084fbf05afebd9f7bfb
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 18:28:11 +0200

    Update mayor.php

--- ejercicio3/mayor.php
+++ ejercicio3/mayor.php
@@ -2,6 +2,7 @@
 $numero1 = $_POST['numero1'];
 $numero2 = $_POST['numero2'];
 $numero3 = $_POST['numero3'];
+//Mediante if y else if se harab todas las comparaciones necesatias
 if ($numero1 == $numero2 && $numero1 == $numero3) {
     echo "Los tres números son iguales.";
 } else if ($numero1 >= $numero2 && $numero1 >= $numero3) {

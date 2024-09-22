d57bc0460197dfcdd989357514d74d01877c0258
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 17:30:24 +0200

    Add files via upload

---
+++ ejercicio4/dias.php
@@ -0,0 +1,28 @@
+<?php
+$diasemana = $_POST['dia'];
+switch ($diasemana) {
+    case 1:
+        echo "Lunes";
+        break;
+    case 2;
+        echo "Martes";
+        break;
+    case 3;
+        echo "Miercoles";
+        break;
+    case 4;
+        echo "Jueves";
+        break;
+    case 5;
+        echo "Viernes";
+        break;
+    case 6;
+        echo "Sabado";
+        break;
+    case 7;
+        echo "Domingo";
+        break;
+    default:
+        echo "Fuerad de rango";
+}
+?>

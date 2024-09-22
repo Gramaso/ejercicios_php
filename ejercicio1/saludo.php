3c2e32e5e77719dff3b963924e9d8c9c5708185a
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 18:25:39 +0200

    Update saludo.php

--- ejercicio1/saludo.php
+++ ejercicio1/saludo.php
@@ -1,5 +1,7 @@
 <?php
+//Creamos las variables, guardando los datos obtenidos del html
 $nombre1 = $_POST['nombre'];
 $apellido1 = $_POST['apellido'];
+//Mostramos por pantalla el resultado
 echo $nombre1. " " .$apellido1;
 ?>

60955def435cef636b6ecabcf3c9ae5578b30854
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 18:31:30 +0200

    Update suma.php

--- ejercicio6/suma.php
+++ ejercicio6/suma.php
@@ -1,6 +1,7 @@
 <?php
 $repeticiones = $_POST['numero'];
 $sumatorio = 0;
+//Este bucle se repetira hasta igualar repeticiones, que es un numero dado por el usuario
 for ($i = 1 ; $i <= $repeticiones ; $i++) {
     $sumatorio = $sumatorio + $i;
 }

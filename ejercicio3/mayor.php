679c2b3ad9a5cc8eee0cc7a40ba848a84f1560b8
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 17:17:56 +0200

    Add files via upload

---
+++ ejercicio3/mayor.php
@@ -0,0 +1,13 @@
+<?php
+$numero1 = $_POST['numero1'];
+$numero2 = $_POST['numero2'];
+$numero3 = $_POST['numero3'];
+if ($numero1 == $numero2 && $numero1 == $numero3) {
+    echo "Los tres números son iguales.";
+} else if ($numero1 >= $numero2 && $numero1 >= $numero3) {
+    echo "El mayor es el : " . $numero1;
+} else if ($numero2 >= $numero1 && $numero2 >= $numero3) {
+    echo "El mayor numero es el: " . $numero2;
+} else
+    echo "El mayor numero es el: " . $numero3;
+?>

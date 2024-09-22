8912f77b96efe3e63ef0ddec1d5bb6b9c5da4f6e
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 18:26:39 +0200

    Update viaje.php

--- ejercicio2/viaje.php
+++ ejercicio2/viaje.php
@@ -1,6 +1,7 @@
 <?php
 $divisor = $_POST['kilometros'];
 $dividendo = $_POST['velocidad'];
+//Dividimos los kilometros por la velovidad
 $tiempo_medio = $divisor / $dividendo;
 echo "El tiempo estimado es: $tiempo_medio horas";
 

916641fdf695d717457a27894b3a437a10913095
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-09-22 18:32:41 +0200

    Update tabla.php

--- ejercicio7/tabla.php
+++ ejercicio7/tabla.php
@@ -1,7 +1,9 @@
 <?php
 $tabla = $_POST['numero'];
+//Marcamos que el bucle termine en 10 este mismo incluido
 for ($i = 1 ; $i <= 10 ; $i++) {
     $reultado = $i*$tabla;
+    //enseñamos el codigo en una tabla
     echo "<table border=1> <tr> <td> $i X $tabla = $reultado </tr> </td> </table>";
      
 }
